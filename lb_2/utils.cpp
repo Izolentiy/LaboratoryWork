b9ed996619cf8a7d8b707c681b56ecf9f15a3f8c
Izolentiy <nima.munkonov@gmail.com>
2023-03-09 23:21:35 +0700

    Добавлена возможность задавать точность в выводе

--- lb_2/utils.cpp
+++ lb_2/utils.cpp
@@ -1,45 +1,32 @@
 #include <iostream>
 #include <random>
+#include <iomanip>
+
+#define SEED 120
 
 /**
  * Функция для вывода элементов массива в консоль
+ * @param vert если [true] выведет элементы в столбик
+ * @param prec задает количество символов после запятой
  */
-void print_array(float arr[], int arr_size, bool verticaly = false) {
-    if (verticaly) {
-        std::cout << '{' << std::endl;
+void print_array(float arr[], int arr_size, bool vert = false, int prec = 2) {
+    using std::cout, std::endl, std::setprecision, std::fixed;
+
+    if (vert) {
+        cout << '{' << endl;
         for (int i = 0; i < arr_size; i++) {
-            std::cout << arr[i] << std::endl;
+            cout << fixed << setprecision(prec) << arr[i] << endl;
         }
-        std::cout << '}' << std::endl;
+        cout << '}' << endl;
         return;
     }
-    std::cout << "{ ";
-    for (int i = 0; i < arr_size; i++) {
-        std::cout << arr[i] << ' ';
-    }
-    std::cout << '}' << std::endl;
-}
 
-// /**
-//  * Функция для вывода элементов массива в консоль
-//  */
-// void print_array(float* ptr_0, int arr_size, bool verticaly = false) {
-//     if (verticaly) {
-//         std::cout << '{' << std::endl;
-//         for (int i = 0; i < arr_size; i++) {
-//             std::cout << ptr_0 + i << std::endl;
-//         }
-//         std::cout << '}' << std::endl;
-//         return;
-//     }
-//     std::cout << "{ ";
-//     for (int i = 0; i < arr_size; i++) {
-//         if (i == arr_size - 1) {
-//             std::cout << ptr_0 + i << " }" << std::endl;
-//         }
-//         else std::cout << ptr_0 + i << ' ';
-//     }
-// }
+    cout << "{ ";
+    for (int i = 0; i < arr_size; i++) {
+        cout << fixed << setprecision(prec) << arr[i] << ' ';
+    }
+    cout << '}' << endl;
+}
 
 /**
  * Функция для заполнения массива числами из заданного полуинтервала [min; max)
@@ -47,11 +34,8 @@ void print_array(float arr[], int arr_size, bool verticaly = false) {
  * @param max верхняя граница значений
  */
 void fill_array(float arr[], int arr_size, float min, float max) {
-    using std::mt19937, std::uniform_real_distribution;
-    
-    mt19937 gen(120);
-
-    uniform_real_distribution distr(min, max);
+    std::mt19937 gen(SEED);
+    std::uniform_real_distribution distr(min, max);
     
     for (int i = 0; i < arr_size; i++) {
         arr[i] = distr(gen);
