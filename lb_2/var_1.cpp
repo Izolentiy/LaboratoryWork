6b0cc11d8ecd489e12cf76e98b2fd24eb019c032
Izolentiy <nima.munkonov@gmail.com>
2023-03-11 19:39:40 +0700

    Сортировка выбором

--- lb_2/var_1.cpp
+++ lb_2/var_1.cpp
@@ -1,5 +1,22 @@
-#include <iostream>
 #include "utils.cpp"
+
+/**
+ * Cортировка выбором элементов с индексами  в отрезке [l, r]
+ * @param l левый индекс
+ * @param r правый индекс
+ */
+void choosing_sort(float a[], int l, int r) {
+    float t = 0;
+    for (int i = l; i < r; i++) {
+        for (int j = i+1; j <= r; j++) {
+            if (a[j] < a[i]) {
+                t = a[j];
+                a[j] = a[i];
+                a[i] = t;
+            }
+        }
+    }
+}
 
 /**
  * Пузырьковая сортировка элементов с индексами  в отрезке [l, r]
@@ -30,7 +47,7 @@ void inserting_sort(float a[], int l, int r) {
     float t = 0;
     for (int i = l; i <= r; i++) {
         for (int j = l; j < i; j++) {
-            std::cout << std::fixed << std::setprecision(0) << a[i] << ' ' << a[j] << std::endl;
+            // std::cout << std::fixed << std::setprecision(0) << a[i] << ' ' << a[j] << std::endl;
             
             if (a[i] < a[j]) {
                 t = a[i];
@@ -39,37 +56,54 @@ void inserting_sort(float a[], int l, int r) {
                 }
                 a[j] = t;
                 
-                print_array(a, 6, 0, 0);
+                // print_array(a, 6, 0, 0);
             }
 
         }
     }
 }
 
-int main() {
-    // using std::cin, std::cout, std::endl;
-
-    // cout << "Enter size of array: ";
-    // int s; cin >> s;
-
-    // cout << "min value: ";
-    // float min; cin >> min;
-
-    // cout << "max value: ";
-    // float max; cin >> max;
-
-    int s = 6;
+void time_complexity_growth_example() {
+    uint16_t s = 32767;
     float min = 0, max = 1024;
     
-    float* a = new float[s];
+    float *a = new float[s];
+    fill_array(a, s, min, max);
+    
+    // Элементанрые алгоритмы сортировки. O(n^2) время выполнения
+    // программы состоит в квадратичной зависимости от количества элементов
+    
+    // bubble_sort(a, 0, s-1);
+    // inserting_sort(a, 0, s-1);
+    // choosing_sort(a, 0, s-1);
+    run_sorting_with_time_measurement(a, s, inserting_sort);
+
+    s = 65535;
+    float *b = new float[s];
+    fill_array(b, s, min, max);
+    
+    run_sorting_with_time_measurement(b, s, inserting_sort);
+
+}
+
+void printing_example() {
+    uint16_t s = 7;
+    float min = 0, max = 1024;
+    
+    float *a = new float[s];
 
     fill_array(a, s, min, max);
     print_array(a, s, false, 0);
-
+    
     // bubble_sort(a, 0, s-1);
     inserting_sort(a, 0, s-1);
+    // choosing_sort(a, 0, s-1);
     
     print_array(a, s, false, 0);
+}
+
+int main() {
+    time_complexity_growth_example();
     
     return 0;
 }
