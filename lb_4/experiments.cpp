2411c15c810b7236bfa690cb74ce76cb1cdd9411
Izolentiy <nima.munkonov@gmail.com>
2023-04-16 01:27:20 +0700

    Эксперименты с массивами указателей на функции

--- lb_4/experiments.cpp
+++ lb_4/experiments.cpp
@@ -1,15 +1,45 @@
-#include "c_string.hpp"
+#include "c_string.h"
 #include <iostream>
 
-int main() {
-    // размер сообщения максимум 18 символов не считая '\0'
-    my::string str(18);
-    uint8_t val = 240;
-    uint8_t r = 0, g = 96, b = 175;
+#define STR_MAX_LEN 32
+
+void c_string_test() {
+    // размер сообщения максимум 19 символов, включая '\n'. Не считая '\0'
+    my::string str(STR_MAX_LEN);
+    uint8_t val = 22;
+    uint8_t r = 255, g = 255, b = 255;
 
     // Построение сообщения
     str + val + " : " + r + ' ' + g + ' ' + b + '\n';
     char* msg = str.as_cstring();
 
     std::cout << msg;
+
+    str.clear();
+    str + ":)";
+    std::cout << str.as_cstring() << std::endl;
+
+    int signed_val = -1245;
+    r = 0; g = 0; b = 0;
+
+    str.clear();
+    str + signed_val + " : " + r + ' ' + g + ' ' + b;
+    std::cout << str.as_cstring();
+}
+
+void (*func_arr[]) (int) = { f1, f2, f3 };
+
+typedef void (*func_t) (int);
+func_t func_arr_2[] = { f1, f2, f3 };
+
+void f1(int n) {}
+void f2(int n) {}
+void f3(int n) {}
+
+void array_of_functions() {
+    (*func_arr[0])();
+}
+
+int main() {
+    c_string_test();
 }
