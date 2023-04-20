c84e0ebbcb80fa139d0734ee48915fae577c7cb8
Izolentiy <nima.munkonov@gmail.com>
2023-04-20 23:49:37 +0700

    №2.5

--- lb_4/task_2.hpp
+++ lb_4/task_2.hpp
@@ -1,17 +1,23 @@
 #pragma once
 #include <fstream>
 #include <iostream>
+#include <cmath>
 
 #include "char_set.h"
-
-typedef void (*subtask_t) (std::ifstream &, std::ofstream &);
+#include "task_typedefs.h"
 
 namespace task_2 {
   void subtask_1(std::ifstream &, std::ofstream &);
   void subtask_2(std::ifstream &, std::ofstream &);
+  void subtask_3(std::ifstream &, std::ofstream &);
+  void subtask_4(std::ifstream &, std::ofstream &);
+  void subtask_5(std::ifstream &, std::ofstream &);
+
+  bool is_digit(char);
+  int32_t parse_from_str(char *, bool, uint8_t);
   
   subtask_t subtasks[] = {
-    subtask_1, subtask_2
+    subtask_1, subtask_2, subtask_3, subtask_4, subtask_5
   };
 }
 
@@ -58,7 +64,8 @@ void task_2::subtask_2(std::ifstream &fin, std::ofstream &fout) {
   // Считывание разделителей из второй строки
   my::char_set delims(16);
   sym = fin.get();
-  while (sym != EOF || sym != '\n') {
+  while (sym != EOF) {
+    if (sym != '\n') break;
     delims.add(sym);
     sym = fin.get();
   }
@@ -73,5 +80,66 @@ void task_2::subtask_2(std::ifstream &fin, std::ofstream &fout) {
     }
     fout << sym;
   }
+}
 
+void task_2::subtask_3(std::ifstream &fin, std::ofstream &fout) {}
+void task_2::subtask_4(std::ifstream &fin, std::ofstream &fout) {}
+
+/**
+ * Дан текст. Найти слова, состоящие из цифр, 
+ * и сумму чисел, которые образуют эти слова.
+ */
+void task_2::subtask_5(std::ifstream &fin, std::ofstream &fout) {
+  char ch = fin.get();
+  std::cout << "subtask 5 " << ch << "\n";
+
+  int32_t num = 0;
+  int64_t sum = 0;
+  bool is_neg = false;
+  char num_s[32] = "";
+  uint8_t dc = 0; // digit count
+  
+  while (ch != EOF) {
+    if (ch == '-') is_neg = true;
+    else
+    if (is_digit(ch)) {
+      do num_s[dc++] = ch;
+      while (is_digit(ch = fin.get()));
+      num_s[dc] = '\0';
+
+      num = parse_from_str(num_s, is_neg, dc);
+      fout << num << ' ';
+      sum += num;
+      dc = 0; is_neg = false;
+    }
+    else is_neg = false;
+    std::cout << ch;
+    ch = fin.get();
+  }
+  fout << "\nSum = " << sum;
+}
+
+bool task_2::is_digit(char ch) {
+  if ('0' <= ch && ch <= '9') return true;
+  return false;
+}
+
+/**
+ * 12345 =
+ * 10000 +
+ *  2000 +
+ *   300 +
+ *    40 +
+ *     5
+ * digit_count = 5 // dc
+ */
+int32_t task_2::parse_from_str(char *str, bool is_neg, uint8_t dc) {
+  int32_t res = 0;
+  uint8_t cur = 0;
+  for (uint8_t i = 0; str[i] != '\0'; ++i) {
+    cur = str[i] - '0';
+    res += cur * std::pow(10, --dc);
+  }
+  if (is_neg) res = ~res + 1;
+  return res;
 }
