901ed3a5837c891082e606ce12e68532ec5e205a
Izolentiy <nima.munkonov@gmail.com>
2023-04-24 21:17:52 +0700

    Эксперименты со структурами и битовыми полями

--- lb_4/experiments.cpp
+++ lb_4/experiments.cpp
@@ -27,19 +27,135 @@ void c_string_test() {
     std::cout << str.as_cstring();
 }
 
+void f1(int n) {}
+void f2(int n) {}
+void f3(int n) {}
+
 void (*func_arr[]) (int) = { f1, f2, f3 };
 
 typedef void (*func_t) (int);
 func_t func_arr_2[] = { f1, f2, f3 };
 
-void f1(int n) {}
-void f2(int n) {}
-void f3(int n) {}
-
 void array_of_functions() {
-    (*func_arr[0])();
+    (*func_arr[0])(1);
+}
+
+// data = {0, 2, 6, 8, 9}
+// ch = 1;
+// ### 1
+// {0, 2, 6, 8, 9}
+//  ^     ^     ^
+//  l     m     h
+// {0, 2, 6, 8, 9}
+//  ^     ^     ^
+//  l     m     h
+// ### 2
+// {0, 2, 6, 8, 9}
+//  ^  ^  ^
+//  l  m  h 
+// ### 3
+// {0, 2, 6, 8, 9}
+//  ^  ^
+//  m  h
+
+// data = {0, 2, 6, 8, 9}
+// ch = 10;
+// ### 1
+// {0, 2, 6, 8, 9}
+//  ^     ^     ^
+//  l     m     h
+// ### 2
+// {0, 2, 6, 8, 9}
+//        ^  ^  ^
+//        l  m  h
+// ### 3
+// {0, 2, 6, 8, 9}
+//           ^  ^
+//           l  h
+
+char data[64];
+uint16_t elem_count = 0;
+int8_t sym = 1;
+
+// Ищет позицию в которую необходимо вставить элемент
+int16_t search(int8_t ch) {
+  int16_t l = 0, m = 0, h = elem_count-1;
+  if (elem_count == 0) return 0;
+  bool flag = false;
+  do {
+    m = (l+h) / 2;
+    if (ch > data[m]) {
+      if (flag)
+        if (ch > data[h]) return h+1;
+        else
+        if (ch < data[h]) return h;
+        else return -1;
+      l = m;
+    }
+    else
+    if (ch < data[m]) {
+      if (flag) return 0;
+      h = m;
+    }
+    else return -1;
+
+    if (h-l <= 1) flag = true;
+  } while (true);
+}
+
+void add(int8_t ch) {
+  int16_t in_pos = search(ch);
+  if (in_pos < 0) return; // ch уже есть
+  for (int i = ++elem_count; i > in_pos; --i) {
+    data[i] = data[i-1];
+  }
+  data[in_pos] = ch;
+}
+
+void search_test() {
+    add(','); std::cout << (int)',' << ",\n";
+    add('_'); std::cout << (int)'_' << "_\n";
+
+    add('-'); std::cout << (int)'-' << "-\n";
+    add('-'); std::cout << (int)'-' << "-\n";
+    add('-'); std::cout << (int)'-' << "-\n";
+    add('+'); std::cout << (int)'+' << "+\n";
+
+    
+    data[7] = '\0';
+    std::cout << elem_count << ' ' << data << '\n';
+}
+
+void bit_field_test() {
+  struct {
+    int32_t size;
+    int16_t num;
+    int16_t id;
+    uint8_t signal_1;
+    uint8_t signal_2;
+  } struct_1; // правильное расположение по адресам
+
+  struct struct_t {
+    uint8_t signal_1;
+    int32_t size;
+    uint8_t signal_2;
+    int16_t id;
+    int16_t num;
+  }; // неправильное расположение по адресам
+
+  struct bit_field_t {
+    uint32_t year: 16; // 65536
+    uint32_t month: 4; // 16
+    uint32_t day: 5; // 32
+  } calendar;
+
+  std::cout << "struct 1: " << sizeof(struct_1) << '\n';
+  std::cout << "struct 2: " << sizeof(struct_t) << '\n';
+  std::cout << "calendar: " << sizeof(calendar) << '\n';
 }
 
 int main() {
-    c_string_test();
+    // search_test();
+    // c_string_test();
+    bit_field_test();
 }
