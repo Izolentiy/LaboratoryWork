484924bae913d89416ffaa8dda12765da65d1b18
Izolentiy <nima.munkonov@gmail.com>
2023-12-06 21:08:57 +0700

    ООП ЛБ5 Деревао. Завершение функционала

--- OOP/lb_4/bin_tree.cpp
+++ OOP/lb_4/bin_tree.cpp
@@ -1,17 +1,47 @@
 #include "bin_tree.h"
 
+my::bin_tree::bin_tree(std::initializer_list<int> list) {
+    for (int item : list) {
+        insert(item);
+    }
+}
+
+my::bin_tree::bin_tree(const bin_tree &other) {
+    copy(other.root);
+}
+
+my::bin_tree::bin_tree(bin_tree &&other) {
+    this->root = other.root;
+    other.root = nullptr;
+}
+
+my::bin_tree &my::bin_tree::operator=(const bin_tree &other) {
+    clear(root);
+    copy(other.root);
+    return *this;
+}
+
+my::bin_tree &my::bin_tree::operator=(bin_tree &&other) {
+    clear(root);
+    this->root = other.root;
+    other.root = nullptr;
+    return *this;
+}
+
 void my::bin_tree::insert(const int &obj) {
     node *to_insert = new node(obj);
-    if (root == nullptr)
+    if (root == nullptr) {
         root = to_insert;
-    else
+    } else {
         insert(to_insert, root);
+    }
 }
 
 void my::bin_tree::insert(node *to_insert, node *subroot) {
     if (subroot->val < to_insert->val) {
         if (subroot->right == nullptr) {
             subroot->right = to_insert;
+            to_insert->parent = subroot;
             return;
         } else {
             insert(to_insert, subroot->right);
@@ -20,6 +50,7 @@ void my::bin_tree::insert(node *to_insert, node *subroot) {
     } else if (subroot->val > to_insert->val) {
         if (subroot->left == nullptr) {
             subroot->left = to_insert;
+            to_insert->parent = subroot;
             return;
         } else {
             insert(to_insert, subroot->left);
@@ -29,73 +60,186 @@ void my::bin_tree::insert(node *to_insert, node *subroot) {
     // values are equal
 }
 
-void my::bin_tree::operator+=(std::initializer_list<int> list) {
-    for (int item : list) {
-        insert(item);
+void my::bin_tree::remove(const int &obj) {
+    node *subroot = move_to(obj);
+    if (subroot == nullptr)
+        return; // not found
+    remove(subroot);
+}
+
+void my::bin_tree::remove(node *to_remove) {
+    if (to_remove->is_leaf()) {
+        dispose(to_remove);
+        return;
+    }
+    node *max_left = get_max(to_remove->left);
+    node *min_right = get_min(to_remove->right);
+    size_t dl = SIZE_MAX, dr = SIZE_MAX; // delta left / right
+    if (max_left != nullptr)
+        dl = std::abs(max_left->val - to_remove->val);
+    if (min_right != nullptr)
+        dr = std::abs(min_right->val - to_remove->val);
+
+    if (dl == dr) {
+        // values are equal
+        if (max_left != nullptr) {
+            to_remove->val = max_left->val;
+            remove(max_left);
+        } else {
+            to_remove->val = min_right->val;
+            remove(min_right);
+        }
+    } else if (dl < dr) {
+        to_remove->val = max_left->val;
+        remove(max_left);
+    } else if (dr < dl) {
+        to_remove->val = min_right->val;
+        remove(min_right);
     }
 }
 
-void my::bin_tree::remove(const int &obj) {}
+void my::bin_tree::dispose(node *to_delete) {
+    node *parent = to_delete->parent;
+    if (parent == nullptr) {
+        // strange scenario
+    } else if (parent->left == to_delete) {
+        parent->left = nullptr;
+    } else if (parent->right == to_delete) {
+        parent->right = nullptr;
+    }
+    delete to_delete;
+}
+
+my::bin_tree::node *my::bin_tree::move_to(const int &obj) {
+    node *temp = root;
+    while (true) {
+        if (temp == nullptr) {
+            break; // not found
+        } else if (temp->val > obj) {
+            temp = temp->left;
+        } else if (temp->val < obj) {
+            temp = temp->right;
+        } else if (temp->val == obj) {
+            break; // found
+        }
+    }
+    return temp;
+}
+
+my::bin_tree::node *my::bin_tree::get_max(node *subroot) {
+    node *temp = subroot;
+    if (subroot != nullptr)
+        while (temp->right != nullptr) {
+            temp = temp->right;
+        }
+    return temp;
+}
+
+my::bin_tree::node *my::bin_tree::get_min(node *subroot) {
+    node *temp = subroot;
+    if (subroot != nullptr)
+        while (temp->left != nullptr) {
+            temp = temp->left;
+        }
+    return temp;
+}
 
 uint16_t my::bin_tree::get_height() {
-    return uint16_t();
+    return get_height(0, root);
+}
+
+uint16_t my::bin_tree::get_height(uint16_t root_distance, node *subroot) {
+    if (subroot == nullptr) {
+        if (root_distance == 0)
+            return root_distance;
+        return --root_distance;
+    }
+    ++root_distance;
+    uint16_t left = get_height(root_distance, subroot->left);
+    uint16_t right = get_height(root_distance, subroot->right);
+    return std::max(left, right);
 }
 
 int my::bin_tree::get_min() {
-    if (root == nullptr)
+    node *min = get_min(root);
+    if (min == nullptr)
         throw std::logic_error("Trying to get min of empty tree");
-    node *temp = root;
-    while (temp->left != nullptr) {
-        temp = temp->left;
-    }
-    return temp->val;
+    return min->val;
 }
 
 int my::bin_tree::get_max() {
-    if (root == nullptr)
-        throw std::logic_error("Trying to get max of empty tree");
-    node *temp = root;
-    while (temp->right != nullptr) {
-        temp = temp->right;
-    }
-    return temp->val;
+    node *max = get_max(root);
+    if (max == nullptr)
+        throw std::logic_error("Trying to get min of empty tree");
+    return max->val;
 }
 
 bool my::bin_tree::contains(const int &obj) {
-    return false;
+    node *temp = move_to(obj);
+    return temp;
 }
 
-my::bin_tree::~bin_tree() {}
+void my::bin_tree::clear(node *subroot) {
+    if (subroot == nullptr) return;
+    clear(subroot->right);
+    clear(subroot->left);
+    delete subroot;
+    subroot = nullptr;
+}
+
+void my::bin_tree::copy(node *subroot) {
+    if (subroot == nullptr) return;
+    insert(subroot->val);
+    copy(subroot->left);
+    copy(subroot->right);
+}
+
+my::bin_tree::~bin_tree() {
+    clear(root);
+}
 
 my::bin_tree::node::node(const int &val, node *left, node *right)
     : val(val), left(left), right(right) {}
 
 std::ostream &my::operator<<(std::ostream &out, bin_tree &tree) {
     tree_printer printer(out, tree.root);
-    printer.print_tree();
+    printer.set_print_depth(5)
+        .draw_tree();
     return out;
 }
 
-void my::tree_printer::print_tree() {
+void my::tree_printer::draw_tree() {
     // TODO: Improve algorithm to exclude overwork
     print_distance = 0;
-    while (print(print_root, 0)) { // print branches
-        out << "\n";
+    while (draw(print_root, 0)) { // print branches
+        *out << "\n";
         is_printing_nodes = true;
-        print(print_root, 0); // print nodes
+        draw(print_root, 0); // print nodes
         is_printing_nodes = false;
-        out << "\n";
+        *out << "\n";
         ++print_distance;
     }
 }
 
-bool my::tree_printer::print(bin_tree::node *root, uint16_t root_distance) {
+void my::tree_printer::print_symmetric() {
+    print_symmetric(print_root);
+}
+
+void my::tree_printer::print_straight() {
+    print_straight(print_root);
+}
+
+void my::tree_printer::print_reversed() {
+    print_reversed(print_root);
+}
+
+bool my::tree_printer::draw(bin_tree::node *root, uint16_t root_distance) {
     if (root_distance == print_distance || root == nullptr) {
-        if (is_printing_nodes) {
-            print_node(root, root_distance);
-        } else {
-            print_branch(root, root_distance);
-        }
+        if (is_printing_nodes)
+            draw_node(root, root_distance);
+        else
+            draw_branch(root, root_distance);
+
         if (root == nullptr)
             return false;
         return true;
@@ -103,49 +247,97 @@ bool my::tree_printer::print(bin_tree::node *root, uint16_t root_distance) {
     if (root_distance < print_distance) {
         // go further
         ++root_distance;
-        bool left = print(root->left, root_distance);
-        bool right = print(root->right, root_distance);
+        bool left = draw(root->left, root_distance);
+        bool right = draw(root->right, root_distance);
         return left || right;
     }
     return false;
 }
 
-void my::tree_printer::print_node(bin_tree::node *root, uint16_t root_distance) {
-    int16_t tab = pow(2, levels_to_print - 1 - root_distance) - 1;
+void my::tree_printer::draw_node(bin_tree::node *root, uint16_t root_distance) {
+    int16_t tab = pow(2, print_depth - 1 - root_distance) - 1;
     if (root == nullptr) {
         print_char(' ', tab * 2);
-        out << ' ';
+        *out << ' ';
         print_char(' ', tab * 2 + 3);
         return;
     }
-    print_char(' ', tab);
-    print_char('_', tab);
-    out << root->val;
-    print_char('_', tab);
-    print_char(' ', tab + 3);
+    if (root->left == nullptr) {
+        print_char(' ', tab * 2);
+    } else {
+        print_char(' ', tab);
+        print_char('_', tab);
+    }
+    *out << root->val;
+    // out << '*';
+    if (root->right == nullptr) {
+        print_char(' ', tab * 2 + 3);
+    } else {
+        print_char('_', tab);
+        print_char(' ', tab + 3);
+    }
 }
 
-void my::tree_printer::print_branch(bin_tree::node *root, uint16_t root_distance) {
+void my::tree_printer::draw_branch(bin_tree::node *root, uint16_t root_distance) {
     if (root_distance > 0) {
-        int16_t tab = pow(2, levels_to_print - 1 - root_distance) - 1;
+        int16_t tab = pow(2, print_depth - 1 - root_distance) - 1;
         print_char(' ', tab * 2);
         if (root == nullptr)
-            out << ' ';
+            *out << ' ';
         else
-            out << '|';
+            *out << '|';
         print_char(' ', tab * 2 + 3);
     }
 }
 
 void my::tree_printer::print_char(char ch, int times) {
     for (int i = 0; i < times; ++i) {
-        out << ch;
+        *out << ch;
     }
 }
 
-void my::tree_printer::set_level_count(uint16_t level) {
-    this->levels_to_print = level;
+void my::tree_printer::print_symmetric(bin_tree::node *subroot) {
+    if (subroot == nullptr) return;
+    print_symmetric(subroot->left);
+    print_node(subroot);
+    print_symmetric(subroot->right);
+}
+
+void my::tree_printer::print_straight(bin_tree::node *subroot) {
+    if (subroot == nullptr) return;
+    print_node(subroot);
+    print_straight(subroot->left);
+    print_straight(subroot->right);
+}
+
+void my::tree_printer::print_reversed(bin_tree::node *subroot) {
+    if (subroot == nullptr) return;
+    print_reversed(subroot->right);
+    print_reversed(subroot->left);
+    print_node(subroot);
+}
+
+void my::tree_printer::print_node(bin_tree::node *node) {
+    *out << ' ' << node->val << ' ';
+}
+
+my::tree_printer &my::tree_printer::set_print_depth(uint16_t level) {
+    this->print_depth = level;
+    return *this;
+}
+
+my::tree_printer &my::tree_printer::set_root(bin_tree::node *root) {
+    this->print_root = root;
+    return *this;
+}
+
+my::tree_printer &my::tree_printer::set_ostream(std::ostream &out) {
+    this->out = &out;
+    return *this;
 }
 
 my::tree_printer::tree_printer(std::ostream &out, bin_tree::node *root)
-    : out(out), print_root(root) {}
+    : out(&out), print_root(root) {}
+
+my::tree_printer::tree_printer(std::ostream &out, bin_tree &tree)
+    : out(&out), print_root(tree.root) {}
--- OOP/lb_4/bin_tree.h
+++ OOP/lb_4/bin_tree.h
@@ -15,13 +15,13 @@ namespace my {
 
 4                    _______________*______________
                     |                              \
-3            _______*_______                  _____ * _____
+3            ______ * ______                  _____ * _____
             |               |                /             \
 2        ___*___         __ 1 __          _ * _           __*__
         |       |       |       |        /     \         /     \
-1      _*_     _*_      *      _*_      *       *       *       *
-      |   |   |   |   |   |   |   |    / \     / \     / \     / \
-0     *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *
+1      _*_     _*      _*_      *       *       *       *       *
+      |       |       |   |    / \     /       / \     / \     / \
+0     *       *       *   *   *   *   *       *   *   *   *   *   *
       |                                               |
      >5                                               2
 
@@ -32,49 +32,69 @@ namespace my {
 */
 
 class my::bin_tree {
-    class node {
-      public:
+    struct node {
+        int val;
+        node *parent = nullptr;
         node *left;
         node *right;
-        int val;
-
         node(const int &val, node *left = nullptr, node *right = nullptr);
+        bool is_leaf() { return left == nullptr && right == nullptr; }
     };
     friend class tree_printer;
     friend std::ostream &my::operator<<(std::ostream &out, bin_tree &tree);
 
   private:
     node *root = nullptr;
-    uint8_t levels_to_print = 4;
     void insert(node *to_insert, node *subroot);
+    void remove(node *to_remove);
+    void dispose(node *to_delete);
+    uint16_t get_height(uint16_t root_distance, node *subroot);
+    node *move_to(const int &obj);
+    node *get_max(node *subroot);
+    node *get_min(node *subroot);
+    void clear(node *subroot);
+    void copy(node *subroot);
 
   public:
     bin_tree() = default;
+    bin_tree(std::initializer_list<int> list);
+    bin_tree(const bin_tree &other);
+    bin_tree(bin_tree &&other);
+    bin_tree &operator=(const bin_tree &other);
+    bin_tree &operator=(bin_tree &&other);
     void insert(const int &obj);
-    void operator+=(std::initializer_list<int> list);
-
     void remove(const int &obj);
+    bool contains(const int &obj);
     uint16_t get_height();
     int get_min();
     int get_max();
-    bool contains(const int &obj);
     ~bin_tree();
 };
 
 class my::tree_printer {
   private:
-    std::ostream &out;
+    std::ostream *out;
     bin_tree::node *print_root;
-    uint16_t levels_to_print = 6;
+    uint16_t print_depth = 5;
     uint16_t print_distance = 0;
     bool is_printing_nodes = false;
-    bool print(bin_tree::node *root, uint16_t root_distance);
-    void print_node(bin_tree::node *root, uint16_t root_distance);
-    void print_branch(bin_tree::node *root, uint16_t root_distance);
+    bool draw(bin_tree::node *root, uint16_t root_distance);
+    void draw_node(bin_tree::node *root, uint16_t root_distance);
+    void draw_branch(bin_tree::node *root, uint16_t root_distance);
     void print_char(char ch, int times);
+    void print_symmetric(bin_tree::node *subroot);
+    void print_straight(bin_tree::node *subroot);
+    void print_reversed(bin_tree::node *subroot);
+    void print_node(bin_tree::node *node);
 
   public:
     tree_printer(std::ostream &out, bin_tree::node *root);
-    void print_tree();
-    void set_level_count(uint16_t level);
+    tree_printer(std::ostream &out, bin_tree &tree);
+    tree_printer &set_ostream(std::ostream &out);
+    tree_printer &set_print_depth(uint16_t depth);
+    tree_printer &set_root(bin_tree::node *root);
+    void draw_tree();
+    void print_symmetric();
+    void print_straight();
+    void print_reversed();
 };
